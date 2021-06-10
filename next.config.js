491868f61ab8f7e2a7251174cf0012e97d9fc23c
Jed Watson <jed.watson@me.com>
2021-06-10 17:33:44 +1000

    Conecting Next to Railway in production

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ module.exports = {
     return [
       {
         source: '/api/graphql',
-        destination: 'http://localhost:3000/api/graphql',
+        destination:
+          process.env.NODE_ENV === 'production'
+            ? 'https://keystone-react-todo-demo-production.up.railway.app'
+            : 'http://localhost:3000/api/graphql',
       },
     ];
   },
